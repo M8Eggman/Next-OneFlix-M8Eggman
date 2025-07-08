f51715b10a12165be491a28daf462f49d15519f8
GoNEAR <cagatay.morgul@outlook.com>
2025-07-08 11:16:49 +0200

    anime slice avec stockage des anime les plus populaire les nouveaute naruto et all

--- src/types/index.ts
+++ src/types/index.ts
@@ -18,3 +18,43 @@ export interface TypeUser {
   watchlist: any[];
   ownedItems: any[];
 }
+
+export interface TypeAnime {
+  mal_id: number;
+  title: string;
+  images: {
+    jpg: {
+      image_url: string;
+    };
+  };
+}
+export interface TypeAnimeResponse {
+  animeList: TypeAnime[];
+  pagination: {
+    last_visible_page: number;
+    has_next_page: boolean;
+    current_page: number;
+  };
+}
+export interface TypeAnimeState {
+  all: TypeAnime[];
+  naruto: TypeAnime[];
+  popular: TypeAnime[];
+  new: TypeAnime[];
+  loadingAll: boolean;
+  loadingNaruto: boolean;
+  loadingPopular: boolean;
+  loadingNew: boolean;
+  errorAll: string | null;
+  errorNaruto: string | null;
+  errorPopular: string | null;
+  errorNew: string | null;
+  pageAll: number;
+  pageNaruto: number;
+  pagePopular: number;
+  pageNew: number;
+  allPagination: TypeAnimeResponse["pagination"] | null;
+  narutoPagination: TypeAnimeResponse["pagination"] | null;
+  popularPagination: TypeAnimeResponse["pagination"] | null;
+  newPagination: TypeAnimeResponse["pagination"] | null;
+}
